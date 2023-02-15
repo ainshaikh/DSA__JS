bddae4ec3d6a87bd282683c52ce227e9eb3202a8
Ain Shaikh <ainsaikhx7@gmail.com>
2023-02-15 10:39:21 +0530

    :tada:optimized soln

--- sumZero.js
+++ sumZero.js
@@ -13,3 +13,27 @@ function getSumZero(array){
 const result = getSumZero([-5, -3, -3, -2, 0, 2, 4, 6, 8]);
 console.log(result);
 // time complexity will be O(n^2) i.e quadratic
+
+
+// OPTIMIZED SOLUTION 
+function findSumPair (array){
+    let left = 0;
+    let right = array.length-1;
+    
+    while(left < right){
+        sum = (array[left] + array[right]);
+        if(sum === 0){
+            return [array[left], array[right]];
+        }
+        else if(sum > 0){
+            right --;
+        }
+        else{
+            left ++;
+        }
+    }
+}
+const results = findSumPair([-5, -3, -3, -2, 0, 2, 4, 6, 8]);
+console.log(results);
+
+// time complexity is o(n) linear
